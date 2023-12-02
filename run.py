5a418403f5b21e90f0fb17c3d43c452c6386385a
qasimushtaq <>
2023-12-02 19:59:31 +0000

    added the main menu function

--- run.py
+++ run.py
@@ -26,3 +26,37 @@ CURRENT_DATE = datetime.date.today()
 # Places and stores the current date into the correct format for when required.
 CURRENT_DATE_FMTED = datetime.datetime.strftime(CURRENT_DATE, "%d/%m/%Y")
 
+def main_menu():
+    """
+    Displays the main menu options for the user
+    to select in order to navigate the application.
+    """
+    clear_tmnl()
+    print("Appointment System - Main menu\n")
+    print("Please select an option below.\n")
+
+    print("(1) Book new appointment.")
+    print("(2) View today's appointments.")
+    print("(3) Search appointments.")
+    print("(4) Cancel appointment.")
+    print("(5) View application instructions.")
+
+    while True:
+        main_menu_ans = input("\n")
+        if main_menu_ans not in ("1", "2", "3", "4", "5"):
+            print("Invalid input.")
+            print("Please choose an option between 1 and 5.")
+        else:
+            break
+
+    if main_menu_ans == ("1"):
+        collect_details()
+    elif main_menu_ans == ("2"):
+        search_date("today")
+    elif main_menu_ans == ("3"):
+        search_menu()
+    elif main_menu_ans == ("4"):
+        cancelation_prompt()
+    elif main_menu_ans == ("5"):
+        app_info()
+       
