a3df499f4180b9f487c1ae5a0045689db0a558d0
qasimushtaq <>
2023-12-02 13:20:04 +0000

    working on sheet to store the data

--- run.py
+++ run.py
@@ -16,3 +16,13 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("appointment-system")
+
+# Global variables for app processes.
+
+# Variable for sheet in the spreadsheet.
+APPTS = SHEET.worksheet("appointments")
+# Stores the current date.
+CURRENT_DATE = datetime.date.today()
+# Places and stores the current date into the correct format for when required.
+CURRENT_DATE_FMTED = datetime.datetime.strftime(CURRENT_DATE, "%d/%m/%Y")
+
